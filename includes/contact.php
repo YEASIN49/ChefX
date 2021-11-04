d76991b5d8dfd86dfbe791a64140b9fe9e9175bb
MD.YEASIN ALI <myeasin49@gmail.com>
2021-11-04 23:15:40 +0600

    contact.php responsiveness done

--- includes/contact.php
+++ includes/contact.php
@@ -5,21 +5,42 @@
         </div>
 
     <div class="contactContent">
-       <div class="socialIcon">
-         <ul>
-            <li class="twitter"><a href=""></a><i class="fab fa-twitter"><p>twitter</p></i></li>
-            <li class="facebook"><a href=""></a><i class="fab fa-facebook-square"><p>facebook</p></i></li>
-            <li class="youtube"><a href=""></a><i class="fab fa-youtube"><p>YouTube</p></i></li>
-            <li class="instagram"><a href=""></a><img src="images/logo/instagram.png" alt=""></li>
-         </ul>
-       </div>
+      <div class="socialIcon">
+        <div class="addressContainer">
+          <h2>Our Location:</h2>
+          <p class="addressContent"><span class="addressNumber">Address 1:</span> <span class="addressDetail">1/2/A xyz tower,Kolabagan road, Dhanmondi, Dhaka-1219</span><span class="mobileNumber">Call: +8801234567890 </span> </p>
+          <p class="addressContent"><span class="addressNumber">Address 2:</span> <span class="addressDetail">1/2/3A abc tower, X Road, Mirpur-2, Dhaka-12xx</span><span class="mobileNumber">Call: +8801234567890 </span> </p>
+          <p class="addressContent"><span class="addressNumber">Address 3:</span> <span class="addressDetail">12/34/C X center, xyz road, Mohammadpur, Dhaka-12xx</span><span class="mobileNumber">Call: +8801234567890 </span> </p>
+        </div>
+        <ul>
+          <h2 style="display:block; width: 100%; margin-bottom: 20px;">Find Us on Social Media!</h2>
+          <li class="twitter"><a href=""></a><i class="fab fa-twitter"><p>twitter</p></i></li>
+          <li class="facebook"><a href=""></a><i class="fab fa-facebook-square"><p>facebook</p></i></li>
+          <li class="youtube"><a href=""></a><i class="fab fa-youtube"><p>YouTube</p></i></li>
+          <li class="instagram"><a href=""></a><img src="images/logo/instagram.png" alt=""></li>
+        </ul>
+      </div>
+      <div class="reservationFormContainer">
+        <h2>Reserve Your Table!</h2>
+        <label>Your Name :</label>
+        <input class="reservationInput" type="text"/>
+        <label>Your Email :</label>
+        <input class="reservationInput"  type="email"/>
+        <label>Mobile Number</label>
+        <input class="reservationInput" type="number"/>
+        <label>Select Time</label>
+        <input class="reservationInput" type="time"/>
+        <label>Select Date</label>
+        <input class="reservationInput" type="date" />
+        <input class="reservationInput" type="submit" name="submit" value='Submit' placeholder="sybmit"/>
+      </div>
     
-         <div id="googleMap" style="width:45vw;height:25vw;background:red;margin-bottom:30px;"></div>
+         <!-- <div id="googleMap" style="width:45vw;height:25vw;background:red;margin-bottom:30px;"></div> -->
     </div>
 </div>
 
 
-<script>
+<!-- <script>
 function myMap() {
 var mapProp= {
   center:new google.maps.LatLng(23.762133,90.415655),
@@ -29,4 +50,4 @@ var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
 </script>
 
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBDoINsGm1gfFnWtlB0Y58-qFXB9hpM7HU&callback=myMap"></script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBDoINsGm1gfFnWtlB0Y58-qFXB9hpM7HU&callback=myMap"></script> -->
